4fbad06a0750662de0b6bf1601621564db0b2105
YabsiraYetwale <yabsirayetwale@gmail.com>
2024-04-02 16:24:10 +0300

    updated

--- client/src/redux/api/index.tsx
+++ client/src/redux/api/index.tsx
@@ -1,9 +1,20 @@
 import axios from "axios";
 const API = axios.create({ baseURL:"http://localhost:3001/api"});
 
-API.interceptors.request.use((req) => {
-  if (localStorage.getItem("InvoiceAuth")) {
-    const token = JSON.parse(localStorage.getItem("InvoiceAuth")).data.token;
+// API.interceptors.request.use((req) => {
+//   if (localStorage.getItem("InvoiceAuth")) {
+//     const token = JSON.parse(localStorage.getItem("InvoiceAuth")).data.token;
+//     req.headers = {
+//       ...req.headers,
+//       Authorization: `Bearer ${token}`,
+//     };
+//   }
+//   return req;
+// });
+API.interceptors.request.use((req:any) => {
+  const authData = localStorage.getItem("InvoiceAuth");
+  if (authData && typeof authData === "string") {
+    const token = JSON.parse(authData).data.token;
     req.headers = {
       ...req.headers,
       Authorization: `Bearer ${token}`,
@@ -13,37 +24,37 @@ API.interceptors.request.use((req) => {
 });
 
 // Auth
-export const signUp = (user) => API.post("/auth/register", user);
-export const signIn = (user) => API.post("/auth/login", user);
+export const signUp = (user:any) => API.post("/auth/register", user);
+export const signIn = (user:any) => API.post("/auth/login", user);
 
 // user profile
 export const fetchCurrentUser = () => API.get("/auth/user/current-user");
 export const fetchUsers = () => API.get("/auth");
-export const fetchUser = (id) => API.get(`/auth/${id}`);
-export const updateUser = (id, user) => API.put(`/auth/${id}`, user);
-export const deleteUser = (id) => API.delete(`/auth/${id}`);
+export const fetchUser = (id:String) => API.get(`/auth/${id}`);
+export const updateUser = (id:String, user:any) => API.put(`/auth/${id}`, user);
+export const deleteUser = (id:String) => API.delete(`/auth/${id}`);
 
 // customer
-export const createCustomer = (customer) => API.post("/clients", customer);
+export const createCustomer = (customer:any) => API.post("/clients", customer);
 export const fetchCustomers = () => API.get("/clients");
-export const fetchCustomer = (id) => API.get(`/clients/${id}`);
-export const updateCustomer = (id, customer) => API.put(`/clients/${id}`, customer);
-export const deleteCustomer = (id) => API.delete(`/clients/${id}`);
+export const fetchCustomer = (id:String) => API.get(`/clients/${id}`);
+export const updateCustomer = (id:String, customer:any) => API.put(`/clients/${id}`, customer);
+export const deleteCustomer = (id:String) => API.delete(`/clients/${id}`);
 
 // Invoice
-export const createInvoice = (invoice) => API.post("/invoices", invoice);
+export const createInvoice = (invoice:any) => API.post("/invoices", invoice);
 export const fetchInvoices = () => API.get("/invoices");
-export const fetchInvoice = (id) => API.get(`/invoices/${id}`);
-export const fetchInvoicesBySearch = (searchQuery) =>API.get(`/invoices?searchQuery=${searchQuery}`);
-export const updateInvoice = (id, invoice) => API.put(`/invoices/${id}`, invoice);
-export const deleteInvoice = (id) => API.delete(`/invoices/${id}`);
+export const fetchInvoice = (id:String) => API.get(`/invoices/${id}`);
+export const fetchInvoicesBySearch = (searchQuery:any) =>API.get(`/invoices?searchQuery=${searchQuery}`);
+export const updateInvoice = (id:String, invoice:any) => API.put(`/invoices/${id}`, invoice);
+export const deleteInvoice = (id:String) => API.delete(`/invoices/${id}`);
 
 // Invoice Status
-export const markInvoiceStatusPaid =   (id) => API.put(`/invoices/${id}/mark-as-paid`);
-export const markInvoiceStatusUnPaid = (id) => API.put(`/invoices/${id}/mark-as-unpaid`);
-export const markInvoiceStatusRead =   (id) => API.put(`/invoices/${id}/mark-as-read`);
+export const markInvoiceStatusPaid =   (id:String) => API.put(`/invoices/${id}/mark-as-paid`);
+export const markInvoiceStatusUnPaid = (id:String) => API.put(`/invoices/${id}/mark-as-unpaid`);
+export const markInvoiceStatusRead =   (id:String) => API.put(`/invoices/${id}/mark-as-read`);
 
 // Item
-export const createItem = (item) => API.post("/items", item);
-export const updateItem = (id,item) => API.put(`/items/${id}`, item);
+export const createItem = (item:any) => API.post("/items", item);
+export const updateItem = (id:String,item:any) => API.put(`/items/${id}`, item);
 
