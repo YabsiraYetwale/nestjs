710d3a87bce669fb29e2516402f602ec15c2ecee
YabsiraYetwale <yabsirayetwale@gmail.com>
2024-04-27 17:08:39 +0300

    upated

--- server/src/invoices/invoices.service.ts
+++ server/src/invoices/invoices.service.ts
@@ -48,7 +48,6 @@ async getAllInvoices(searchQuery: string, query: Query) {
 
   const invoices = await this.prismaService.invoices.findMany({
     where: whereCondition,
-    // include: { line_items: true, client: true, },
     include: { line_items: true, client: true,creator: true, },
   });
 
@@ -61,7 +60,7 @@ async getAllInvoices(searchQuery: string, query: Query) {
   async getOneInvoice(id: string) {
     const invoice = await this.prismaService.Invoices.findUnique({
       where: id,
-      include: { line_items: true, client: true,creator: true, },
+      include: { line_items: true, client: true,creator: true,company: true, },
     });
     if (!invoice) {
       throw new HttpException("Invoice doesn't exist", 404)
@@ -70,92 +69,23 @@ async getAllInvoices(searchQuery: string, query: Query) {
     }
   } 
 
-  // async createInvoice(createInvoiceDto: CreateInvoiceDto, validatedUser: ValidatedUser) {
-  //   const { total_amount, line_items, client,creator,company, ...post } = createInvoiceDto;
-
-  //   const totalAmount = line_items?.reduce((total, item) => {
-  //     return total + item.quantity * item.unit_price;
-  //   }, 0);
-
-  //   const lastInvoice = await this.prismaService.Invoices.findFirst({
-  //     orderBy: { invoice_number: 'desc' },
-  //   });
-
-  //   let invoiceNumber = 'INV-001';
-  //   if (lastInvoice && lastInvoice.invoice_number) {
-  //     const lastNumber = parseInt(lastInvoice.invoice_number.split('-')[1]);
-  //     invoiceNumber = `INV-${(lastNumber + 1).toString().padStart(3, '0')}`;
-  //   }
-    
-  //   const newInvoice = await this.prismaService.Invoices.create({
-  //     data: {
-  //       invoice_number: invoiceNumber,
-  //       total_amount: totalAmount,
-  //       client: {
-  //         create: {
-  //           name: client.name,
-  //           billing_address: client.billing_address,
-  //           shipping_address: client.shipping_address,
-  //           shipping_city: client.shipping_city,
-  //           shipping_state: client.shipping_state,
-  //           shipping_zip: client.shipping_zip,
-  //           shipping_country: client.shipping_country,
-  //           contact_person: client.contact_person,
-  //           email: client.email,
-  //           phone: client.phone,
-  //         },
-  //       },
-  //       creator: {
-  //         connect: {
-  //           id: validatedUser.id,
-  //         },
-  //       },
-  //       // company: {
-  //       //   connect: {
-  //       //     id: validatedUser.company_id,
-  //       //   },
-  //       // },
-        
-  //       ...post,
-  //       line_items: {
-  //         create: line_items?.map((item) => ({
-  //           description: item.description,
-  //           quantity: item.quantity,
-  //           unit_price: item.unit_price,
-  //           tax_rate: item.tax_rate,
-  //         })),
-  //       },
-  //     },
-  //     // include: {
-  //     //   line_items: true,
-  //     // },
-  //     include: {
-  //       line_items: true,
-  //       client: true,
-  //       creator: true,
-  //       company: true,
-  //     },
-  //   });
-
-  //   return newInvoice;
-  // }
   async createInvoice(createInvoiceDto: CreateInvoiceDto, validatedUser: ValidatedUser) {
-    const { total_amount, line_items, client, creator, company, ...post } = createInvoiceDto;
-  
+    const { total_amount, line_items, client,creator,company, ...post } = createInvoiceDto;
+
     const totalAmount = line_items?.reduce((total, item) => {
       return total + item.quantity * item.unit_price;
     }, 0);
-  
+
     const lastInvoice = await this.prismaService.Invoices.findFirst({
       orderBy: { invoice_number: 'desc' },
     });
-  
+
     let invoiceNumber = 'INV-001';
     if (lastInvoice && lastInvoice.invoice_number) {
       const lastNumber = parseInt(lastInvoice.invoice_number.split('-')[1]);
       invoiceNumber = `INV-${(lastNumber + 1).toString().padStart(3, '0')}`;
     }
-  
+    
     const newInvoice = await this.prismaService.Invoices.create({
       data: {
         invoice_number: invoiceNumber,
@@ -174,19 +104,17 @@ async getAllInvoices(searchQuery: string, query: Query) {
             phone: client.phone,
           },
         },
-        creator: creator
-          ? {
-              connect: {
-                id: validatedUser.id,
-              },
-            }
-          : undefined,
-        // company: {
-        //   connect: {
-        //     id: validatedUser.company_id,
-        //   },
-        // },
-  
+        creator: {
+          connect: {
+            id: validatedUser.id,
+          },
+        },
+        company: {
+          connect: {
+            id: validatedUser.company_id,
+          },
+        },
+        
         ...post,
         line_items: {
           create: line_items?.map((item) => ({
@@ -197,19 +125,17 @@ async getAllInvoices(searchQuery: string, query: Query) {
           })),
         },
       },
-      // include: {
-      //   line_items: true,
-      // },
       include: {
         line_items: true,
         client: true,
-        creator: creator ? true : false,
+        creator: true,
         company: true,
       },
     });
-  
+
     return newInvoice;
   }
+ 
   
 async updateInvoice(id: string, updateInvoiceDto: UpdateInvoiceDto) {
     const {total_amount,client, line_items, ...post } = updateInvoiceDto;
