c6c282a4b4f2d6138f53078075dbe920d13bb580
YabsiraYetwale <yabsirayetwale@gmail.com>
2024-06-03 10:49:50 +0300

    commented deleteUnverifiedUsers

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
     const user = await this.prisma.user.findUnique({
       where: { id: userId },
       include: {
-        roles: true,
+        // roles: true,
         permissions: {
           select: {
             role: {
@@ -35,39 +35,39 @@ export class UserService {
 
   // deleted unverified users
 
-  async deleteUnverifiedUsers() {
-    try {
-      const thirtyDaysAgo = addDays(new Date(), -30);
+  // async deleteUnverifiedUsers() {
+  //   try {
+  //     const thirtyDaysAgo = addDays(new Date(), -30);
 
-      const unverifiedUsers = await this.prisma.user.findMany({
-        where: {
-          emailVerified: null,
-          createdAt: {
-            lt: thirtyDaysAgo,
-          },
-        },
-      });
+  //     const unverifiedUsers = await this.prisma.user.findMany({
+  //       where: {
+  //         emailVerified: null,
+  //         createdAt: {
+  //           lt: thirtyDaysAgo,
+  //         },
+  //       },
+  //     });
 
-      for (const user of unverifiedUsers) {
-        await this.prisma.user.delete({
-          where: {
-            id: user.id,
-          },
-        });
-        console.log(`Deleted unverified user with ID ${user.id}`);
-      }
+  //     for (const user of unverifiedUsers) {
+  //       await this.prisma.user.delete({
+  //         where: {
+  //           id: user.id,
+  //         },
+  //       });
+  //       console.log(`Deleted unverified user with ID ${user.id}`);
+  //     }
 
-      console.log('Deletion process completed.');
-    } catch (error) {
-      console.error('Error occurred while deleting unverified users:', error);
-    }
-  }
+  //     console.log('Deletion process completed.');
+  //   } catch (error) {
+  //     console.error('Error occurred while deleting unverified users:', error);
+  //   }
+  // }
 
-  @Cron(CronExpression.EVERY_5_SECONDS)
-  handleUnverifiedUsers() {
-    console.log('Unverified users schedularly deleted');
-    this.deleteUnverifiedUsers();
-  }
+  // @Cron(CronExpression.EVERY_5_SECONDS)
+  // handleUnverifiedUsers() {
+  //   console.log('Unverified users schedularly deleted');
+  //   this.deleteUnverifiedUsers();
+  // }
 
 
 
